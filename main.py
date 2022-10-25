ae50ac78255aefd8c7e3f1b061bdcf051f809803
TehyaYi <jinjurhee@gmail.com>
2022-10-25 07:43:49 -0800

    add scheduler

--- main.py
+++ main.py
@@ -3,14 +3,20 @@
 import config
 import pymongo
 import datetime
+import sched
+import time
 
 client = pymongo.MongoClient(f"mongodb+srv://raspi:{config.password}"
                              f"@cluster0.tev7xup.mongodb.net/?retryWrites=true&w=majority",
                              server_api=pymongo.server_api.ServerApi('1'))
 db = client.test
 
-def main():
+s = sched.scheduler(time.time, time.sleep)
+
+
+def update_database(sc):
     """Send some sensor data to a mongo db."""
+    print("doing something")
     print(db.list_collection_names())
     # TODO: make new collection if collection for curr not made
     first_collection = db.test_collection
@@ -25,6 +31,14 @@ def main():
     }
 
     result = first_collection.insert_one(machine)
+    sc.enter(5, 1, update_database, (sc,))
+
+
+def main():
+    """Send data every 5 minutes."""
+    s.enter(5, 1, update_database, (s,))
+    s.run()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
