28d608e7cd52d3295f9ebe7b6c47678636190e03
TehyaYi <jinjurhee@gmail.com>
2022-10-29 12:36:08 -0800

    use hostname as collection name

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import pymongo
 import datetime
 import sched
 import time
+import socket
 
 client = pymongo.MongoClient(f"mongodb+srv://raspi:{config.password}"
                              f"@cluster0.tev7xup.mongodb.net/?retryWrites=true&w=majority",
                              server_api=pymongo.server_api.ServerApi('1'))
 db = client.test
-
-s = sched.scheduler(time.time, time.sleep)
+SCHEDULER = sched.scheduler(time.time, time.sleep)
+PI_NAME = socket.gethostname()
 
 
 def update_database(sc):
@@ -19,25 +20,24 @@ def update_database(sc):
     print("doing something")
     print(db.list_collection_names())
     # TODO: make new collection if collection for curr not made
-    first_collection = db.test_collection
-    num = 5  # random number for testing
+    collection = db[f"{PI_NAME}"]
 
     machine = {
-        "id": str(num),
+        "id": str(PI_NAME),
         "date": str(datetime.datetime.utcnow()),
         "data1": "test 1",
         "data2": "test 2",
         "data3": "test 3",
     }
 
-    result = first_collection.insert_one(machine)
+    result = collection.insert_one(machine)
     sc.enter(5, 1, update_database, (sc,))
 
 
 def main():
     """Send data every 5 minutes."""
-    s.enter(5, 1, update_database, (s,))
-    s.run()
+    SCHEDULER.enter(5, 1, update_database, (SCHEDULER,))
+    SCHEDULER.run()
 
 
 # Press the green button in the gutter to run the script.
